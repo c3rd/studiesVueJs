9a47037094ec79662b064cfc8cddc998dc694505
Celio Maroja <c3rd.dev@gmail.com>
2023-04-25 11:51:13 +0200

    adding tags in component
    Signed-off-by: Celio Maroja <c3rd.dev@gmail.com>

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,22 +1,18 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
 
-    components: { Assignment },
+    components: { Assignment, AssignmentTags },
     template: `
     <section v-if="assignments.length">
         <h1>{{ title }}</h1>
         
-        <div class="flex gap-2">
-            <button
-                @click="currentTag = tag" 
-                v-for="tag in tags"
-                class="border rounded px-1 py-px text-xs"
-                :class="{
-                    'border-blue-500 text-blue-500': tag === currentTag
-                }"
-                >{{ tag }}</button>
-        </div>
+        <assignment-tags 
+            @change="currentTag = $event"
+            :initial-tags="assignments.map(a => a.tag)" 
+            :current-tag = currentTag 
+        />
         <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
             <assignment 
                 v-for="assignment in filteredAssignments" 
@@ -43,9 +39,6 @@ export default {
             }
 
             return this.assignments.filter(a => a.tag === this.currentTag);
-        },
-        tags() {
-            return ['all', ...new Set(this.assignments.map(a => a.tag))];
         }
     }
 }
